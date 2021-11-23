bc94ac02c4f6be5532f43343b259ed1242145876
Saim Rizvi <shr2mz@gmail.com>
2021-11-23 11:24:04 -0500

    commit tree ea508bb887e1990d9463457a4bb3aef404d51f25

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+    
+
+   def clickbait
+    
+        if !title
+            errors.add(:title, "No")
+        else
+
+        unless   title.include? ( "You Won't Believe"||  "Secret" ||"Guess" ||  "Top ")
+                errors.add(:title, "No")
+         end
+        end
+            
+    end
+
+
+    
 end
